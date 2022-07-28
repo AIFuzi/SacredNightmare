e8ae266ff7b864bf6fc2eae1c9f0abac42e492fa
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-07-28 20:32:51 +0300

    Item info

--- Source/SacredNightmare/Private/Items/ItemActor.cpp
+++ Source/SacredNightmare/Private/Items/ItemActor.cpp
@@ -1,8 +1,14 @@
 #include "Items/ItemActor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 AItemActor::AItemActor()
 {
-	
+	ItemBoxCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("ItemBoxCollision"));
+	ItemBoxCollision->SetupAttachment(RootComponent);
+
+	ItemStaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemStaticMesh"));
+	ItemStaticMesh->SetupAttachment(ItemBoxCollision);
 }
 
 void AItemActor::BeginPlay()
--- Source/SacredNightmare/Public/Items/ItemActor.h
+++ Source/SacredNightmare/Public/Items/ItemActor.h
@@ -4,6 +4,25 @@
 #include "GameFramework/Actor.h"
 #include "ItemActor.generated.h"
 
+USTRUCT(BlueprintType)
+struct FItemStruct
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	FName ItemName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	int MiningCondition;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	int ItemCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	float RegenRate;
+	
+};
+
 UCLASS()
 class SACREDNIGHTMARE_API AItemActor : public AActor
 {
@@ -15,4 +34,13 @@ public:
 
 	virtual void BeginPlay() override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ItemActor")
+	class UBoxComponent* ItemBoxCollision;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="ItemActor")
+	class UStaticMeshComponent* ItemStaticMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="ItemInfo")
+	FItemStruct ItemStruct;
+	
 };
