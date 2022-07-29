232d3888067a5252465228051069676f18638efd
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-07-29 12:57:43 +0300

    Add item to current stack

--- Source/SacredNightmare/Private/Components/InventoryComponent.cpp
+++ Source/SacredNightmare/Private/Components/InventoryComponent.cpp
@@ -1,7 +1,52 @@
 #include "Components/InventoryComponent.h"
+#include "Net/UnrealNetwork.h"
 
 UInventoryComponent::UInventoryComponent()
 {
 
 }
 
+void UInventoryComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(UInventoryComponent, InventoryArray);
+}
+
+TArray<FItemStruct> UInventoryComponent::GetInventoryInfo()
+{
+	return InventoryArray;
+}
+
+void UInventoryComponent::AddItemToInventory(FItemStruct ItemAdded)
+{
+	Server_AddItemToInventory(ItemAdded);
+}
+
+void UInventoryComponent::Server_AddItemToInventory_Implementation(FItemStruct ItemAdded)
+{
+	if(GetOwnerRole() == ROLE_Authority)
+	{
+		if(InventoryArray.Num() > 0)
+		{
+			int itemIndex = 0;
+			bool bIsItemFound = false;
+			
+			for(int i = 0; i < InventoryArray.Num(); i++)
+			{
+				if(InventoryArray[i].ItemName == ItemAdded.ItemName)
+				{
+					itemIndex = i;
+					bIsItemFound = true;
+					break;
+				}
+			}
+
+			if(bIsItemFound) InventoryArray[itemIndex].ItemCount = InventoryArray[itemIndex].ItemCount + ItemAdded.ItemCount;
+			else InventoryArray.Add(ItemAdded);
+		}
+		else InventoryArray.Add(ItemAdded);
+	}
+}
+
+bool UInventoryComponent::Server_AddItemToInventory_Validate(FItemStruct ItemAdded) { return true; }
