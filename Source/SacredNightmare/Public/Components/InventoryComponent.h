5355c7581eaec1008b0a092f08d07b19bd4aa233
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-07-29 12:01:44 +0300

    Item test add to inventory

--- Source/SacredNightmare/Public/Components/InventoryComponent.h
+++ Source/SacredNightmare/Public/Components/InventoryComponent.h
@@ -5,16 +5,29 @@
 #include "Items/ItemStructure.h"
 #include "InventoryComponent.generated.h"
 
-
 UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SACREDNIGHTMARE_API UInventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
-
+	
 public:	
-
+	
 	UInventoryComponent();
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Inventory")
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UFUNCTION(BlueprintPure, Category="Inventory")
+	TArray<FItemStruct> GetInventoryInfo();
+
+	UFUNCTION(BlueprintCallable, Category="Inventory")
+	void AddItemToInventory(FItemStruct ItemAdded);
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="Inventory")
+	void Server_AddItemToInventory(FItemStruct ItemAdded);
+
+private:
+
+	UPROPERTY(Replicated)
 	TArray<FItemStruct> InventoryArray;
+	
 };
