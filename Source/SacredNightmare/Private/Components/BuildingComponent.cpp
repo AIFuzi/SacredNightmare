0a1b531c79d122ad071eacf8679370850f767ed5
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-07-30 20:55:58 +0300

    Spawn & destroy preview building

--- Source/SacredNightmare/Private/Components/BuildingComponent.cpp
+++ Source/SacredNightmare/Private/Components/BuildingComponent.cpp
@@ -1,5 +1,7 @@
 #include "Components/BuildingComponent.h"
 #include "Components/InventoryComponent.h"
+#include "Building/BuildingActor.h"
+#include "Components/BoxComponent.h"
 
 UBuildingComponent::UBuildingComponent()
 {
@@ -37,5 +39,59 @@ void UBuildingComponent::FindItemForBuilding(UInventoryComponent* InventoryCompo
 		Items = ItemsForBuild;
 	}
 }
+
+void UBuildingComponent::SpawnPreviewBuilding(TSubclassOf<class ABuildingActor> BuildingClass, UMaterialInstanceDynamic* PreviewMaterial)
+{
+	Server_SpawnPreviewBuilding(BuildingClass, PreviewMaterial);
+}
+
+void UBuildingComponent::Server_SpawnPreviewBuilding_Implementation(TSubclassOf<ABuildingActor> BuildingClass, UMaterialInstanceDynamic* PreviewMaterial)
+{
+	const FVector SpawnLoc (0.f, 0.f, 0.f);
+	const FRotator SpawnRot(0.f, 0.f, 0.f);
+
+	FActorSpawnParameters SpawnParameters;
+	SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	PreviewBuildingObject = GetWorld()->SpawnActor<ABuildingActor>(BuildingClass, SpawnLoc, SpawnRot, SpawnParameters);
+	if(GetOwner() && PreviewBuildingObject)
+	{
+		GetWorld()->GetTimerManager().SetTimer(UpdatePreviewObjectLocationTimer, this, &UBuildingComponent::UpdatePreviewObjectLocation, 0.01, true);
+
+		PreviewDynamic = PreviewBuildingObject->BuildingMesh->CreateDynamicMaterialInstance(0, PreviewMaterial);
+	}
+}
+
+bool UBuildingComponent::Server_SpawnPreviewBuilding_Validate(TSubclassOf<ABuildingActor> BuildingClass, UMaterialInstanceDynamic* PreviewMaterial) { return true; }
+
+void UBuildingComponent::DestroyPreviewBuilding()
+{
+	Server_DestroyPreviewBuilding();
+}
+
+void UBuildingComponent::Server_DestroyPreviewBuilding_Implementation()
+{
+	if(PreviewBuildingObject)
+    	{
+    		GetWorld()->GetTimerManager().ClearTimer(UpdatePreviewObjectLocationTimer);
+    		
+    		PreviewBuildingObject->Destroy();
+    		PreviewBuildingObject = nullptr;
+    	}
+}
+
+bool UBuildingComponent::Server_DestroyPreviewBuilding_Validate() { return true; }
+
+void UBuildingComponent::UpdatePreviewObjectLocation() const
+{
+	const FVector NewLocationXY = GetOwner()->GetActorLocation() + GetOwner()->GetActorForwardVector() * 200.f;
+	const FVector NewLocationZ = GetOwner()->GetActorLocation() + PreviewBuildingObject->BuildingCollision->GetScaledBoxExtent().Z / 2.f;
+	PreviewBuildingObject->SetActorLocation(FVector(NewLocationXY.X, NewLocationXY.Y, NewLocationZ.Z));
+}
+
+
+
+
+
 
 
--- Source/SacredNightmare/Public/Components/BuildingComponent.h
+++ Source/SacredNightmare/Public/Components/BuildingComponent.h
@@ -6,7 +6,6 @@
 #include "Items/ItemStructure.h"
 #include "BuildingComponent.generated.h"
 
-
 UCLASS(Blueprintable, BlueprintType, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class SACREDNIGHTMARE_API UBuildingComponent : public UActorComponent
 {
@@ -17,5 +16,27 @@ public:
 	UBuildingComponent();
 	
 	UFUNCTION(BlueprintPure, Category="Building")
-	static void FindItemForBuilding(class UInventoryComponent* InventoryComponent, TArray<FBuildingStruct> NeedItems, bool& IsItemFound, TArray<FItemStruct>& Items);
+	void FindItemForBuilding(class UInventoryComponent* InventoryComponent, TArray<FBuildingStruct> NeedItems, bool& IsItemFound, TArray<FItemStruct>& Items);
+
+	UFUNCTION(BlueprintCallable, Category="Building")
+	void SpawnPreviewBuilding(TSubclassOf<class ABuildingActor> BuildingClass, UMaterialInstanceDynamic* PreviewMaterial);
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="Building_RPC")
+	void Server_SpawnPreviewBuilding(TSubclassOf<class ABuildingActor> BuildingClass, UMaterialInstanceDynamic* PreviewMaterial);
+
+	UFUNCTION(BlueprintCallable, Category="Building")
+	void DestroyPreviewBuilding();
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="Building_RPC")
+	void Server_DestroyPreviewBuilding();
+
+private:
+
+	FTimerHandle UpdatePreviewObjectLocationTimer;
+	
+	ABuildingActor* PreviewBuildingObject;
+	UMaterialInstanceDynamic* PreviewDynamic;
+
+	void UpdatePreviewObjectLocation() const;
+	
 };
