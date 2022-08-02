6e66c868d2c4bed61505bed0274ef4f2ea257a53
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-08-02 16:20:44 +0300

    Create grid manager class

---
+++ Source/SacredNightmare/Private/Objects/GridManager.cpp
@@ -0,0 +1,6 @@
+#include "Objects/GridManager.h"
+
+AGridManager::AGridManager()
+{
+
+}
---
+++ Source/SacredNightmare/Public/Objects/GridManager.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GridManager.generated.h"
+
+UCLASS()
+class SACREDNIGHTMARE_API AGridManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AGridManager();
+
+
+
+};
