e6d454e9dc297cae60d7166967a308943c1c6904
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-08-21 02:21:19 +0300

    Spawn and attach weapon to player

--- Source/SacredNightmare/Private/Components/WeaponComponent.cpp
+++ Source/SacredNightmare/Private/Components/WeaponComponent.cpp
@@ -1,5 +1,6 @@
 #include "Components/WeaponComponent.h"
 #include "Characters/PlayerCharacter.h"
+#include "Weapons/WeaponActor.h"
 
 UWeaponComponent::UWeaponComponent()
 {
@@ -17,4 +18,34 @@ APlayerCharacter* UWeaponComponent::GetPlayerOwner() const
 {
 	return PlayerOwner;
 }
+
+void UWeaponComponent::SpawnAndAttachWeapon(TSubclassOf<AWeaponActor> WeaponClass)
+{
+	Server_SpawnAndAttachWeapon(WeaponClass);
+}
+
+void UWeaponComponent::Server_SpawnAndAttachWeapon_Implementation(TSubclassOf<AWeaponActor> WeaponClass)
+{
+	if(WeaponClass && PlayerOwner)
+	{
+		const FVector SpawnLoc = FVector(0.f, 0.f, 0.f);
+		const FRotator SpawnRot = FRotator(0.f, 0.f, 0.f);
+
+		FActorSpawnParameters SpawnParameters;
+		SpawnParameters.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+		if(AWeaponActor* SpawnedWeapon = GetWorld()->SpawnActor<AWeaponActor>(WeaponClass, SpawnLoc, SpawnRot, SpawnParameters))
+		{
+			CurrentWeapon = SpawnedWeapon;
+			
+			SpawnedWeapon->SetActorEnableCollision(false);
+			SpawnedWeapon->AttachToComponent(PlayerOwner->GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, "SK_WeaponRHand");
+
+			SpawnedWeapon->WeaponOwner = PlayerOwner;
+			SpawnedWeapon->OnRep_WeaponOwner();
+		}
+	}
+}
+
+bool UWeaponComponent::Server_SpawnAndAttachWeapon_Validate(TSubclassOf<AWeaponActor> WeaponClass) { return true; }
 
--- Source/SacredNightmare/Public/Components/WeaponComponent.h
+++ Source/SacredNightmare/Public/Components/WeaponComponent.h
@@ -20,12 +20,21 @@ protected:
 
 public:
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Weapon")
+	class AWeaponActor* CurrentWeapon;
+
 	UFUNCTION(BlueprintPure, Category="WeaponComponent")
 	class APlayerCharacter* GetPlayerOwner() const;
+
+	UFUNCTION(BlueprintCallable, Category="WeaponComponent")
+	void SpawnAndAttachWeapon(TSubclassOf<class AWeaponActor> WeaponClass);
+
+	UFUNCTION(Unreliable, Server, WithValidation, Category="WeaponComponent")
+	void Server_SpawnAndAttachWeapon(TSubclassOf<AWeaponActor> WeaponClass);
 
 private:
 
 	UPROPERTY()
 	APlayerCharacter* PlayerOwner;
-		
+	
 };
